f4cb98d600e57dfc169d753857875509af8782b8
Kaushik Gopal <code@kaush.co>
2013-02-05 09:00:12 -0800

    Rails project creation files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanban::Application.config.secret_token = '349668c85dadc5fb0559f721611edd8cfe6ddd9d1057fe98924d0b46d356c387ad5be0be16b0fe9a7936f539a1fd9e826c369f429fb7d975adcab8d60861c660'
