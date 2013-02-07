851a7a2ee6495ed8e246d833263e120eef3e9186
Kaushik Gopal <code@kaush.co>
2013-02-06 23:40:24 -0800

    merging factory files into 1

--- spec/factories/news_items.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-require 'faker'
-
-FactoryGirl.define do
-  factory :news_item do
-    content Faker::Lorem.paragraph
-  end
-end
