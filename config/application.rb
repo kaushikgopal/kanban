d75d4eeb7b8e66fc21126e2df239e1dbbf69028a
Kaushik Gopal <code@kaush.co>
2013-02-06 20:39:45 -0800

    adding commented out line for importing path into assets line

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,7 @@ module Kanban
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    # config.assets.paths +=%W("#{Rails.root}/app/assets/stylesheets/vendor" "#{Rails.root}/app/assets/javascripts/vendor")
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
