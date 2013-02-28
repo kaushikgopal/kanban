973395037e65cb8e8f391e8b76f107d8d5e8cef1
Kaushik Gopal <code@kaush.co>
2013-02-28 09:57:48 -0800

    adding test string as variabel - good practice

--- spec/features/stream_spec.rb
+++ spec/features/stream_spec.rb
@@ -1,20 +1,23 @@
 require 'spec_helper'
 
 describe "Stream" do
+  before(:all) do
+    @random_string = "PhkOq6Fc57v8DlbcQFGxc0Or70i"
+  end
   it "should be able to add a news_item" do
     visit stream_path
     click_link 'Add news item'
    	expect{
-      fill_in 'news_item_content', with: "Test PhkOq6Fc57v8DlbcQFGxc0Or70i"
+      fill_in 'news_item_content', with: "Test #{@random_string}"
 	   	click_button "Add News Item"
     }.to change(NewsItem,:count).by(1)
     visit stream_path
-    page.should have_content "Test PhkOq6Fc57v8DlbcQFGxc0Or70i"
+    page.should have_content "Test #{@random_string}"
   end
 
   it "should be able to delete news_item" do
     # TODO control this with Cancan
-    news_item = create(:news_item, content: "Test PhkOq6Fc57v8DlbcQFGxc0Or70i")
+    news_item = create(:news_item, content: "Test #{@random_string}")
     visit stream_path
     expect{
       within "#ni_#{news_item.id}" do
@@ -22,7 +25,7 @@ describe "Stream" do
       end
     }.to change(NewsItem, :count).by(-1)
     # sleep(1)
-    should_not have_content "Test PhkOq6Fc57v8DlbcQFGxc0Or70i"
+    should_not have_content "Test #{@random_string}"
   end
 
 end
