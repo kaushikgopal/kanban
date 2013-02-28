21042384b52c0d3305ff58f1d6a5e795628bb3b2
Kaushik Gopal <code@kaush.co>
2013-02-28 10:00:58 -0800

    random string - good practice

--- spec/features/news_spec.rb
+++ spec/features/news_spec.rb
@@ -2,14 +2,17 @@ require 'spec_helper'
 
 describe	'News Management' do
 	context 'List all News' do
+		before(:all) do
+	    @random_string = "PhkOq6Fc57v8DlbcQFGxc0Or70i"
+	  end
 		it "shows the list of news items" do
 			visit news_path
 			page.should have_content 'NewsItems'
 		end
 		it 'should have a new item created' do
-			create(:news_item, content: "Testing page should have this content - 12349532")
+			create(:news_item, content: "Testing page should have this content - #{@random_string }")
 			visit news_path
-			page.should have_content 'Testing page should have this content - 12349532'
+			page.should have_content "Testing page should have this content - #{@random_string }"
 		end
 	end
 
