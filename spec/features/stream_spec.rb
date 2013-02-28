24145e2a3729fd7e548b999356d96ae068f0863e
Kaushik Gopal <code@kaush.co>
2013-02-27 23:53:32 -0800

    bullet proofing request specs with an exact phrase match

--- spec/features/stream_spec.rb
+++ spec/features/stream_spec.rb
@@ -1,27 +1,28 @@
 require 'spec_helper'
 
 describe "Stream" do
-  it "should have an option to add a news_item" do
+  it "should be able to add a news_item" do
     visit stream_path
     click_link 'Add news item'
    	expect{
-      fill_in 'news_item_content', with: "This is a test news item"
+      fill_in 'news_item_content', with: "Test PhkOq6Fc57v8DlbcQFGxc0Or70i"
 	   	click_button "Add News Item"
     }.to change(NewsItem,:count).by(1)
     visit stream_path
-    page.should have_content "This is a test news item"
+    page.should have_content "Test PhkOq6Fc57v8DlbcQFGxc0Or70i"
   end
 
-  it "should have an option to delete news_item" do
+  it "should be able to delete news_item" do
     # TODO control this with Cancan
-    news_item = create(:news_item, content: "Test ABC1234987")
+    news_item = create(:news_item, content: "Test PhkOq6Fc57v8DlbcQFGxc0Or70i")
     visit stream_path
     expect{
       within "#ni_#{news_item.id}" do
         click_link 'Delete'
       end
     }.to change(NewsItem, :count).by(-1)
-    page.should_not have_content "Test ABC1234987"
+    # sleep(1)
+    should_not have_content "Test PhkOq6Fc57v8DlbcQFGxc0Or70i"
   end
 
 end
